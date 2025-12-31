de2c5219c5d469cd3ebd5c7446100f1fd39581d3
Matteo Terreran <matteo.terreran@gmail.com>
2025-12-31 16:03:05 +0100

    Add option for a simulated RGBD camera

--- ur_handeye_simulation/launch/spawn_pattern.launch.py
+++ ur_handeye_simulation/launch/spawn_pattern.launch.py
@@ -14,16 +14,43 @@ def generate_launch_description():
 
     file = PathJoinSubstitution([simulation_package, "models", "checker_default", "model.sdf"])
 
+    # ---- Declare launch arguments ----
+    declare_x = DeclareLaunchArgument('x', default_value='1.8', description='Spawn position X')
+    declare_y = DeclareLaunchArgument('y', default_value='0.4', description='Spawn position Y')
+    declare_z = DeclareLaunchArgument('z', default_value='1.01', description='Spawn position Z')
+    declare_roll  = DeclareLaunchArgument('roll', default_value='0.0', description='Spawn position roll')
+    declare_pitch = DeclareLaunchArgument('pitch', default_value='0.0', description='Spawn position pitch')
+    declare_yaw   = DeclareLaunchArgument('yaw', default_value='0.0', description='Spawn position yaw')
+
 
     spawn_model_description = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([
-            PathJoinSubstitution([FindPackageShare('ros_gz_sim'),
-                                   'launch',
-                                   'gz_spawn_model.launch.py'])]),
-        launch_arguments=[('file', file),
-                          ('entity_name', "pattern"),
-                          ('allow_renaming', "true"),
-        ])
+            PathJoinSubstitution([
+                FindPackageShare('ros_gz_sim'),
+                'launch',
+                'gz_spawn_model.launch.py'
+                ])
+        ]),
+        launch_arguments={
+            'file': file,
+            'entity_name': 'pattern',
+            'allow_renaming': 'true',
+            'x': LaunchConfiguration('x'),
+            'y': LaunchConfiguration('y'),
+            'z': LaunchConfiguration('z'),
+            'roll': LaunchConfiguration('roll'),
+            'pitch': LaunchConfiguration('pitch'),
+            'yaw': LaunchConfiguration('yaw'),
+        }.items()
+    )
     
-    return LaunchDescription([spawn_model_description])
+    return LaunchDescription([
+        declare_x,
+        declare_y,
+        declare_z,
+        declare_roll,
+        declare_pitch,
+        declare_yaw,
+        spawn_model_description
+    ])
     
